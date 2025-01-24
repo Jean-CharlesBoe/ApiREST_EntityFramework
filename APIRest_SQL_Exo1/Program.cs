cbd2deb51286b6f09ebaa607482915920e58fa82
Jean-CharlesBoe <jeancharles.boe059@gmail.com>
2025-01-24 10:09:36 +0100

    Utilisation du swagger en env de DEV

--- APIRest_SQL_Exo1/Program.cs
+++ APIRest_SQL_Exo1/Program.cs
@@ -17,13 +17,11 @@ builder.Services.AddDbContext<BiblioContext>(options => options.UseSqlite("Data 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-    
-//}
-
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
